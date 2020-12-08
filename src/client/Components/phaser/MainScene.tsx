0b770904b6ba9fde15f3c01ab480d14703cec379
Aminoor Rashid <arashid717@gmail.com>
2020-12-08 17:07:15 -0500

    refactoring code

--- src/client/Components/phaser/MainScene.tsx
+++ src/client/Components/phaser/MainScene.tsx
@@ -3,6 +3,20 @@ import { Player } from "./Player";
 import { GridControls } from "./GridControls";
 import { GridPhysics } from "./GridPhysics";
 import { createMonsterAnims } from "./EnemyAnimations";
+import Weapon from "./Weapon";
+
+//declare the gameState globally
+interface looseObj {
+  [key: string]: number;
+}
+const gameState: looseObj = {
+  health: 20, // TODO: Decrease every time an enemy collides with player && increase every time player walks over/attacks a heart
+  score: 0, // TODO: Increase every time an enemy collides with sword animation or walks over/attacks a gem
+};
+
+// interface beatMeter {
+//   beat: Phaser.GameObjects.Image,
+// }
 
 export default class MainScene extends Phaser.Scene {
   //scalar config of the tile sizes
@@ -15,6 +29,9 @@ export default class MainScene extends Phaser.Scene {
   private gridControls?: GridControls;
   private gridPhysics?: GridPhysics;
 
+  // private beatMap: Array<beatMeter> = []
+
+  //working on refactoring this!!!!
   private beat1?: Phaser.GameObjects.Image;
   private beat2?: Phaser.GameObjects.Image;
   private beat3?: Phaser.GameObjects.Image;
@@ -23,10 +40,16 @@ export default class MainScene extends Phaser.Scene {
   private beat6?: Phaser.GameObjects.Image;
   private beat7?: Phaser.GameObjects.Image;
   private beat8?: Phaser.GameObjects.Image;
+
   private background?: Phaser.GameObjects.Image;
+
+  private textGroup?: Phaser.GameObjects.Group;
+  private weapon?: Weapon;
+  private gameScene?: Phaser.Scenes.ScenePlugin;
 
   constructor() {
     super("main-scene");
+    this.death = this.death.bind(this);
   }
 
   public preload(): void {
@@ -55,36 +78,15 @@ export default class MainScene extends Phaser.Scene {
       "assets/enemies/lizard.json"
     );
 
-    //converts the song's BPM to milliseconds
-    const msPerBeat = (60 / this.BPM) * 1000;
-    const speedCursorNeedsToMove = (msPerBeat * 2.5) / 100;
-
-    //sets the rythm of the gameplay based on the available BPM
-    setInterval(() => {
-      this.gridPhysics?.moveToBeat();
-    }, msPerBeat);
-
-    setInterval(() => {
-      this.beat1!.x += 2.5;
-      if (this.beat1!.x >= 200) this.beat1!.x = 100;
-      this.beat2!.x += 2.5;
-      if (this.beat2!.x >= 300) this.beat2!.x = 200;
-      this.beat3!.x += 2.5;
-      if (this.beat3!.x >= 400) this.beat3!.x = 300;
-      this.beat4!.x += 2.5;
-      if (this.beat4!.x >= 500) this.beat4!.x = 400;
-      this.beat5!.x += 2.5;
-      if (this.beat5!.x >= 600) this.beat5!.x = 500;
-      this.beat6!.x += 2.5;
-      if (this.beat6!.x >= 700) this.beat6!.x = 600;
-      this.beat7!.x += 2.5;
-      if (this.beat7!.x >= 800) this.beat7!.x = 700;
-      this.beat8!.x += 2.5;
-      if (this.beat8!.x >= 900) this.beat8!.x = 800;
-    }, speedCursorNeedsToMove);
+    //load the sword into the map
+    this.load.spritesheet("sword", "assets/sprites/smallSword.png", {
+      frameWidth: 48,
+      frameHeight: 48,
+    });
   }
 
   public create(): void {
+    console.log(this.BPM);
     //creates the map we want by parsing the JSON file and filling with sprites
     const dungeonMap = this.make.tilemap({ key: "temple-map" });
     dungeonMap.addTilesetImage("Temple of TS", "tiles");
@@ -96,40 +98,20 @@ export default class MainScene extends Phaser.Scene {
       layer.scale = 3;
     }
 
-    //load character into game
-    const playerSprite = this.physics.add.sprite(0, 0, "player");
-    playerSprite.setDepth(2);
+    // for (let i = 1; i <= 8; i++) {
+    //   const alpha = 0.2;
+    //   const location = 100 * i;
 
-    //creates enemy - lizard
-    const monster = this.add.sprite(
-      0,
-      0,
-      "monster",
-      "lizard_m_idle_anim_f0.png"
-    );
+    //   const beat: Phaser.GameObjects.Image = this.add
+    //   .image(location, 550, "music")
+    //   .setScrollFactor(0)
+    //   .setDepth(4);
 
-    // this.cameras.main.startFollow(playerSprite);
-    this.cameras.main.startFollow(playerSprite);
+    //   const objectToPush: beatMeter = {beat: beat, alpha: alpha}
+    //   this.beatMap.push(objectToPush)
+    // }
 
-    this.gridPhysics = new GridPhysics(
-      //arguments for new Player are (spritesheet, characterIndex, startTilePosX, startTilePosY)
-      new Player(playerSprite, 0, 29, 57),
-      dungeonMap
-    );
-    this.gridControls = new GridControls(this.input, this.gridPhysics);
-
-    //gridphysics for enemy to spawn at particular spot
-    new GridPhysics(
-      //arguments for new Player are (spritesheet, characterIndex, startTilePosX, startTilePosY)
-      new Player(monster, 4, 28, 48), //coordinates where enemy spawns
-      dungeonMap
-    );
-
-    //refactored animations to separate file - EnemyAnimations.tsx
-    createMonsterAnims(this.anims);
-
-    monster.anims.play("lizard-run");
-
+    //working on refactoring this!!!!
     this.beat1 = this.add
       .image(100, 550, "music")
       .setScrollFactor(0)
@@ -167,12 +149,161 @@ export default class MainScene extends Phaser.Scene {
       .setScrollFactor(0)
       .setDepth(3);
 
+    //working on refactoring this!!!!
     this.beat1.alpha = 0.2;
     this.beat8.alpha = 0.2;
     this.beat2.alpha = 0.5;
     this.beat7.alpha = 0.5;
     this.beat3.alpha = 0.8;
     this.beat6.alpha = 0.8;
+
+    //converts the song's BPM to milliseconds
+    const msPerBeat = (60 / this.BPM) * 1000;
+    const msForOneBeat = (msPerBeat * 2.5) / 100;
+
+    //sets the rythm of the gameplay based on the available BPM
+    const playerTimer = this.time.addEvent({
+      delay: msPerBeat,
+      callback: () => console.log(this.gridPhysics?.moveToBeat()),
+      loop: true,
+    });
+
+    //working on refactoring this!!!!
+    const beatTimer = this.time.addEvent({
+      delay: msForOneBeat,
+      callback: () => {
+        this.beat1!.x += 2.5;
+        if (this.beat1!.x >= 200) this.beat1!.x = 100;
+        this.beat2!.x += 2.5;
+        if (this.beat2!.x >= 300) this.beat2!.x = 200;
+        this.beat3!.x += 2.5;
+        if (this.beat3!.x >= 400) this.beat3!.x = 300;
+        this.beat4!.x += 2.5;
+        if (this.beat4!.x >= 500) this.beat4!.x = 400;
+        this.beat5!.x += 2.5;
+        if (this.beat5!.x >= 600) this.beat5!.x = 500;
+        this.beat6!.x += 2.5;
+        if (this.beat6!.x >= 700) this.beat6!.x = 600;
+        this.beat7!.x += 2.5;
+        if (this.beat7!.x >= 800) this.beat7!.x = 700;
+        this.beat8!.x += 2.5;
+        if (this.beat8!.x >= 900) this.beat8!.x = 800;
+      },
+      loop: true,
+    });
+
+    const beatTimer = this.time.addEvent({
+      delay: msForOneBeat,
+      callback: () => {
+        this.beat1!.x += 2.5;
+        if (this.beat1!.x >= 200) this.beat1!.x = 100;
+        this.beat2!.x += 2.5;
+        if (this.beat2!.x >= 300) this.beat2!.x = 200;
+        this.beat3!.x += 2.5;
+        if (this.beat3!.x >= 400) this.beat3!.x = 300;
+        this.beat4!.x += 2.5;
+        if (this.beat4!.x >= 500) this.beat4!.x = 400;
+        this.beat5!.x += 2.5;
+        if (this.beat5!.x >= 600) this.beat5!.x = 500;
+        this.beat6!.x += 2.5;
+        if (this.beat6!.x >= 700) this.beat6!.x = 600;
+        this.beat7!.x += 2.5;
+        if (this.beat7!.x >= 800) this.beat7!.x = 700;
+        this.beat8!.x += 2.5;
+        if (this.beat8!.x >= 900) this.beat8!.x = 800;
+      },
+      loop: true,
+    });
+
+    //load character into game
+    const playerSprite = this.physics.add.sprite(0, 0, "player");
+    playerSprite.setDepth(2);
+
+    //creates enemy - lizard
+    const monster = this.physics.add.sprite(
+      0,
+      0,
+      "monster",
+      "lizard_m_idle_anim_f0.png"
+    );
+
+    // this.cameras.main.startFollow(playerSprite);
+    this.cameras.main.startFollow(playerSprite);
+
+    //Style for our text boxes
+    const textStyle = {
+      fill: "#FFFFFF",
+      fontSize: "20px",
+      backgroundColor: "#000000",
+    };
+
+    //Group for the text boxes - may not need them grouped, but just in case it is useful later
+    this.textGroup = this.add.group();
+
+    //Player health text box, includes styling and scroll factor of 0 to make the box follow the camera dynamically
+    const healthText = this.add
+      .text(775, 15, `Player health: ${gameState.health}`, textStyle)
+      .setScrollFactor(0);
+
+    //Player score text box, includes styling and scroll factor of 0 to make the box follow the camera dynamically
+    const scoreText = this.add
+      .text(15, 15, `Player Score: ${gameState.score}`, textStyle)
+      .setScrollFactor(0);
+
+    //Add text boxes to group
+    this.textGroup.add(healthText);
+    this.textGroup.add(scoreText);
+
+    //Set textGroup to third layer
+    this.textGroup.setDepth(3);
+
+    this.gridPhysics = new GridPhysics(
+      //arguments for new Player are (spritesheet, characterIndex, startTilePosX, startTilePosY)
+      new Player(playerSprite, 0, 29, 57),
+      dungeonMap
+    );
+    this.gridControls = new GridControls(this.input, this.gridPhysics);
+
+    //gridphysics for enemy to spawn at particular spot
+    new GridPhysics(
+      //arguments for new Player are (spritesheet, characterIndex, startTilePosX, startTilePosY)
+      new Player(monster, 4, 28, 48), //coordinates where enemy spawns
+      dungeonMap
+    );
+    monster.setDepth(2);
+
+    //refactored animations to separate file - EnemyAnimations.tsx
+    createMonsterAnims(this.anims);
+
+    monster.anims.play("lizard-run");
+
+    // Create the hitbox and bring it to sprite layer
+    const hitbox = this.physics.add.sprite(0, 0, "sword");
+    hitbox.setDepth(2);
+
+    // Add the overlap physics to destroy an enemy
+    this.physics.add.collider(monster, hitbox, () => {
+      monster.destroy();
+      gameState.score += 1;
+      scoreText.setText(`Player Score: ${gameState.score}`);
+    });
+
+    // Add collider physics between an enemy and player
+    this.physics.add.collider(
+      playerSprite,
+      monster,
+      this.collisionCheck(() => {
+        gameState.health -= 1;
+        healthText.setText(`Player Health: ${gameState.health}`);
+
+        if (gameState.health <= 0) {
+          this.death(); // Currently only turns off the physics for the player and doesn't stop player from moving.
+        }
+      })
+    );
+
+    // Create the weapon functionality
+    this.weapon = new Weapon(this.input, false, hitbox, playerSprite);
   }
 
   //Phaser calls update with 2 arguments: time and delta.
@@ -181,5 +312,39 @@ export default class MainScene extends Phaser.Scene {
   public update(_time: number, delta: number): void {
     this.gridControls?.update();
     this.gridPhysics?.update(delta);
+    this.weapon?.update();
+  }
+
+  public death(): void {
+    //pauses the game. TODO: add click button to start a new game.
+    this.gameScene?.pause();
+    this.add
+      .text(325, 200, "YOU DIED", {
+        fontSize: "80px",
+        fill: "#8b0000",
+        fontStyle: "bold",
+      })
+      .setScrollFactor(0)
+      .setDepth(3);
+    this.add
+      .text(430, 350, "Click to Restart", { fontSize: "15px", fill: "#FFFFFF" })
+      .setScrollFactor(0)
+      .setDepth(3);
+    // const deathScene = this.scene.get([MainScene]);
+    // deathScene.scene.restart();
+  }
+
+  //this function makes sure callback on collider (enemy vs player) fires only once
+  //player loses one life
+  //TODO: define interface for function type and pass it to the below function - work in progress...
+  public collisionCheck(callback?: any, context = this): any {
+    let once = false;
+
+    return (...args: any[]) => {
+      if (!once) {
+        once = true;
+        callback?.apply(context, args);
+      }
+    };
   }
 }
