a97e5eba90f762d6c8e18d112f46ebe13625f078
Aminoor Rashid <arashid717@gmail.com>
2020-12-16 13:00:52 -0500

    merging with dev

--- src/client/Components/Spotify_Components/SongSelection/SelectorComponents/inGameSongs.tsx
+++ src/client/Components/Spotify_Components/SongSelection/SelectorComponents/inGameSongs.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { ISongQueueState } from "../../../../store/Reducers/songQueueReducer/type";
-import { removeSongFromQueue } from "../../../../store/Reducers/songQueueReducer/asyncActions";
 
 interface IqueueItemProps {
   qItem: ISongQueueState;
@@ -10,22 +8,12 @@ interface IqueueItemProps {
 export const inGameSongs: React.FC<IqueueItemProps> = ({
   qItem,
 }): JSX.Element => {
-  const dispatch = useDispatch();
-
   return (
     <div className="individal-queue-song">
       <img src={qItem.img} />
       <div className="queue-song">
         Name: {qItem.name} <br />
         BPM: {qItem.BPM} <br />
-        <div>
-          <button
-            className="nes-btn is-error"
-            onClick={() => dispatch(removeSongFromQueue(qItem))}
-          >
-            Remove Song
-          </button>
-        </div>
       </div>
     </div>
   );
